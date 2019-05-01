05b8a1c359eb7a6f7cc1e6aaa1ec161225b8123e
Alex Frank <alexfrankcodes@gmail.com>
2019-05-01 13:44:41 -0700

    Reconfigured base project to use MongoDB for user database

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("nodejs_login", "root", "", {
+const sequelize = new Sequelize("employees", "root", "", {
   host: "localhost",
   dialect: "mysql",
   operatorsAliases: false,
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const cors = require("cors");
 
+// Initialize app
 const app = express();
 
 app.get("/", (req, res) => res.send("Server is running"));
 
+// Run on port 5000 locally, otherwise port indicate by environment variable
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
